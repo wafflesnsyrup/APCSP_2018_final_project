217ec365e08f8b57253869ae7ddaba37d2c4feba
wafflesnsyrup <40150876+wafflesnsyrup@users.noreply.github.com>
2018-06-11 12:18:13 -0700

    Update albumview.php

--- albumview.php
+++ albumview.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
   <head>
-    <title>Aquatic Album View</title>
+    <title>Lorem ipsum Album View</title>
     <?php require($_SERVER['DOCUMENT_ROOT'] . "/system/head.php"); ?>
   </head>
   <body>
